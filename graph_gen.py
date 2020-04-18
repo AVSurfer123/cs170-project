674a609922ec2e12abd815207af85fe7a15c9c19
Aditya Ramkumar <ramkumar.aditya@gmail.com>
2020-04-18 16:29:02 -0700

    Script to generate random graphs

---
+++ graph_gen.py
@@ -0,0 +1,12 @@
+import random 
+
+def generate_graph(num_vertices):
+    print(num_vertices)
+    for i in range(num_vertices - 1):
+        print(i, i + 1, round(random.uniform(0, 100), 3))
+    for i in range(random.randrange(0, num_vertices)):
+    	vertex1, vertex2 = random.randrange(0, num_vertices), random.randrange(0, num_vertices)
+    	if vertex1 != vertex2:
+    		print(vertex1, vertex2, round(random.uniform(0, 100), 3))
+
+generate_graph(10)
