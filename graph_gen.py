b686bf0dcec97e2657de55433bbd29616ca57cc7
Ashwin Vangipuram <ashwinvangipuram@gmail.com>
2020-04-18 20:34:37 -0400

    Added labels to vis. Increased minimum edges

--- graph_gen.py
+++ graph_gen.py
@@ -8,7 +8,7 @@ def generate_graph(num_vertices):
     G = nx.Graph()
     for i in range(num_vertices - 1):
         G.add_edge(i, i + 1, weight=round(random.uniform(0, 100), 3))
-    for i in range(random.randrange(0, num_vertices)):
+    for i in range(random.randrange(num_vertices//8, num_vertices)):
     	vertex1, vertex2 = random.randrange(0, num_vertices), random.randrange(0, num_vertices)
     	if vertex1 != vertex2 and not G.has_edge(vertex1, vertex2):
     		G.add_edge(vertex1, vertex2, weight=round(random.uniform(0, 100), 3))
--- visualize_graph.py
+++ visualize_graph.py
@@ -3,8 +3,14 @@ import matplotlib.pyplot as plt
 from parse import read_input_file
 import sys
 
-def visualize_graph(G):
-    nx.draw_networkx(G)
+def visualize_graph(G, round_labels=True):
+    pos = nx.spring_layout(G)
+    nx.draw_networkx(G, pos)
+    labels = nx.get_edge_attributes(G, 'weight')
+    if round_labels:
+        for key in labels:
+            labels[key] = round(labels[key])
+    nx.draw_networkx_edge_labels(G, pos, labels)
     plt.show()
 
 if __name__ == '__main__':
