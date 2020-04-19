6e1678c91e58f47b0a70a0f3250a10992ac8f47b
Aditya Ramkumar <aditya.ramkumar@berkeley.edu>
2020-04-18 17:07:46 -0700

    Fixed bug with duplicate edges

--- graph_gen.py
+++ graph_gen.py
@@ -6,7 +6,7 @@ def generate_graph(num_vertices):
         print(i, i + 1, round(random.uniform(0, 100), 3))
     for i in range(random.randrange(0, num_vertices)):
     	vertex1, vertex2 = random.randrange(0, num_vertices), random.randrange(0, num_vertices)
-    	if vertex1 != vertex2:
+    	if vertex1 != vertex2 or vertex1 == vertex2 + 1 or vertex1 + 1 == vertex2:
     		print(vertex1, vertex2, round(random.uniform(0, 100), 3))
 
 generate_graph(int(sys.argv[1]))
