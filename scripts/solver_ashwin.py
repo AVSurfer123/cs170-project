1f96fb68b6a1d89b7eac4d37fd0e0d10161462a3
Aditya Ramkumar <aditya.ramkumar@berkeley.edu>
2020-05-03 12:09:23 -0700

    Ran Ashwin's solver and optimized outputs

--- scripts/solver_ashwin.py
+++ scripts/solver_ashwin.py
@@ -1,47 +1,50 @@
 import networkx as nx
-from parse import read_input_file, write_output_file, write_input_file
-from utils import is_valid_network, average_pairwise_distance, average_pairwise_distance_fast
-import functools, glob, random, sys, os
+from parse import read_input_file, write_output_file
+from utils import is_valid_network, average_pairwise_distance_fast
+import functools, glob, random, sys, os, time
+import random
 
 
 def prune_vertices_rec(G, T, min_pairwise_distance, depth=5):
     if depth == 0:
         return T, min_pairwise_distance
-    for vertex in G.nodes():
-        if not T.has_node(vertex):
+    best_T = T
+    for vertex in T.nodes():
+        # Need this check since we reassign best_T within the loop
+        if not best_T.has_node(vertex):
             continue
-        if len(T[vertex]) == 1:
-            T_copy = T.copy()
+        if len(best_T[vertex]) == 1:
+            T_copy = best_T.copy()
             T_copy.remove_node(vertex)
             if is_valid_network(G, T_copy):
                 pairwise_distance = average_pairwise_distance_fast(T_copy)
                 if pairwise_distance < min_pairwise_distance:
-                    min_pairwise_distance, T = pairwise_distance, T_copy
-                rec_T, rec_pairwise_distance = prune_vertices_rec(G, T_copy, min_pairwise_distance, depth - 1)
-                if rec_pairwise_distance < min_pairwise_distance:
-                    min_pairwise_distance, T = rec_pairwise_distance, rec_T
+                    best_T, min_pairwise_distance = T_copy, pairwise_distance
+    best_T, min_pairwise_distance = prune_vertices_rec(G, best_T, min_pairwise_distance, depth - 1)
+
     return T, min_pairwise_distance
 
-def prune_vertices_deep(G, T):
+
+def prune_vertices_iter(G, T, depth):
     min_pairwise_distance = average_pairwise_distance_fast(T)
-    change = True
-    while change:
-        change = False
-        for vertex in G.nodes():
-            if not T.has_node(vertex):
-                continue
+    for _ in range(depth):
+        for vertex in T.nodes:
             if len(T[vertex]) == 1:
                 T_copy = T.copy()
                 T_copy.remove_node(vertex)
                 if is_valid_network(G, T_copy):
                     pairwise_distance = average_pairwise_distance_fast(T_copy)
                     if pairwise_distance < min_pairwise_distance:
-                        change = True
-                        T.remove_node(vertex)
-                        min_pairwise_distance = pairwise_distance
+                        T, min_pairwise_distance = T_copy, pairwise_distance
+    return T
+
+
 
 def min_spt(G, pruner_depth):
-    min_cost, T = float('inf'), None
+    T = nx.minimum_spanning_tree(G)
+    # T_curr, _ = prune_vertices_rec(G, T, average_pairwise_distance_fast(T), pruner_depth)
+    T = prune_vertices_iter(G, T, T.number_of_nodes())
+    min_cost = average_pairwise_distance_fast(T)
     for vertex in G.nodes():
         length, all_paths = nx.single_source_dijkstra(G, vertex)
         T_curr = nx.Graph()
@@ -51,24 +54,19 @@ def min_spt(G, pruner_depth):
             for i in range(len(path) - 1):
                 T_curr.add_edge(path[i], path[i + 1], weight=G[path[i]][path[i + 1]]["weight"])
 
-        T
+        start = time.time()
         T_curr, _ = prune_vertices_rec(G, T_curr, average_pairwise_distance_fast(T_curr), pruner_depth)
-        prune_vertices_deep(G, T_curr)
-        cost = average_pairwise_distance_fast(T_curr)
+        # print(f'prune rec took {time.time() - start}')
+        start = time.time()
+        T_curr = prune_vertices_iter(G, T_curr, T_curr.number_of_nodes()//2)
+        # print(f'prune iter took {time.time() - start}')
 
+
+        cost = average_pairwise_distance_fast(T_curr)
         if cost < min_cost:
             min_cost, T = cost, T_curr
-    return T
 
-'''def prune_vertices_push(G, T):
-    for vertex in G.nodes():
-        if not T.has_node(vertex):
-            continue
-        neighbors = G.neighbors(vertex)
-        for neighbor in neighbors:
-            if not T.has_node(neighbor):
-                continue
-            T_copy = T.copy()'''
+    return T, min_cost
 
 
 
@@ -80,7 +78,7 @@ def solve(G, pruner_depth=4):
     Returns:
         T: networkx.Graph
     """
-    #All vertices connected to 1 check
+    # Check if all vertices connected to 1
     for vertex in G.nodes():
         if len(list(G.neighbors(vertex))) == G.number_of_nodes() - 1:
             T = nx.Graph()
@@ -88,14 +86,19 @@ def solve(G, pruner_depth=4):
             return T
 
     #Find the minimum shortest paths tree
-    T = min_spt(G, pruner_depth)
+    start = time.time()
+    T, min_pairwise_distance = min_spt(G, pruner_depth)
+    print(f'min spt took {time.time() - start}')
 
-    #Prune the unecessary edges/vertices
-    #min_pairwise_distance = average_pairwise_distance_fast(T)
-    #prune_vertices_push(G, T)
-    #T, _ = prune_vertices_rec(G, T, min_pairwise_distance, pruner_depth)
-    #prune_vertices_deep(G, T)
+    start = time.time()
+    T, _ = prune_vertices_rec(G, T, min_pairwise_distance, pruner_depth)
+    print(f'prune rec took {time.time() - start}')
+    start = time.time()
+    T = prune_vertices_iter(G, T, T.number_of_nodes())
+    print(f'prune iter took {time.time() - start}')
+
     return T
+
 
 # Here's an example of how to run your solver.
 
@@ -111,28 +114,25 @@ if __name__ == '__main__':
         paths = [inputs]
     print(paths)
     count = 1
+    to_redo = ['']
     for path in paths:
-        '''safe = False
+        G = read_input_file(path)
+        safe = False
         for element in to_redo:
             if element in path:
                 print(element, path)
                 safe = True
         if not safe:
-            continue'''
+            continue
 
-        G = read_input_file(path)
-        T = solve(G, pruner_depth = 0)
-        min_pairwise_distance = average_pairwise_distance_fast(T)
-        for i in range(1, 10):
-            curr_T = solve(G, pruner_depth = i)
-            pairwise_distance = average_pairwise_distance_fast(curr_T)
-            print(i, pairwise_distance)
-            if pairwise_distance < min_pairwise_distance:
-                min_pairwise_distance = pairwise_distance
-                T = curr_T
-
+        if 'small' in path:
+            T = solve(G, pruner_depth=20)
+        elif 'large' in path:
+            T = solve(G, pruner_depth=2)
+        else:
+            T = solve(G, pruner_depth = 3)
         assert is_valid_network(G, T)
         graph_name = os.path.basename(path).split(".")[0]
-        print(count, f"Average  pairwise distance for {graph_name}: {average_pairwise_distance(T)}")
+        print(count, f"Average  pairwise distance for {graph_name}: {average_pairwise_distance_fast(T)}")
         count += 1
         write_output_file(T, os.path.join(output_dir, f"{graph_name}.out"))
